5729836c62a15477cd0e3cfcaaf5295dd14ed6be
Joshua Gates <joshua.gates@student.nua.ac.uk>
2019-03-14 14:30:23 +0000

    Revert "Revert "Initial commit""
    This reverts commit 6bf570d993d174e5343ca60f7d4c3292053aeaec.

---
+++ Source/BloodGame/RedBloodCell.cpp
@@ -0,0 +1,131 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RedBloodCell.h"
+
+
+// Sets default values
+ARedBloodCell::ARedBloodCell()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	this->RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	this->CellSprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("Cell Sprite"));
+	CellSprite->SetupAttachment(RootComponent);
+
+}
+
+// Called every time anything changes
+void ARedBloodCell::OnConstruction(const FTransform& Transform)
+{
+
+	if (!VerticleSwaySeverity) { VerticleSwaySeverity = 20; }
+	if (!HorizontalSwaySeverity) { HorizontalSwaySeverity = 20; }
+	uint8 RandomVerticleDirection = FMath::RandRange(0, 1);
+	uint8 RandomHorizontalDirection = FMath::RandRange(0, 1);
+	if (RandomVerticleDirection)
+	{
+		VerticleSwayDestination = VerticleSwaySeverity;
+	}
+	else
+	{
+		VerticleSwayDestination = VerticleSwaySeverity * -1;
+	}
+	if (RandomHorizontalDirection)
+	{
+		HorizontalSwayDestination = HorizontalSwaySeverity;
+	}
+	else
+	{
+		HorizontalSwayDestination = HorizontalSwaySeverity * -1;
+	}
+
+	if (ApplyChanges) {
+		if (this->CellSprites[0])
+		{
+			Sprite = CellSprites[FMath::RandRange(0, CellSprites.Num() - 1)];
+			CellSprite->SetSprite(Sprite);
+
+			CellSprite->TranslucencySortPriority = 60;
+		}else
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, TEXT("Please allocate a sprite"));
+		}
+
+		ApplyChanges = false;
+	}
+
+	if (CellMovementDirection == MovementDirectionEnum::LeftMovement || CellMovementDirection == MovementDirectionEnum::RightMovement) { HorizontalSway = false; }
+	if (CellMovementDirection == MovementDirectionEnum::UpMovement || CellMovementDirection == MovementDirectionEnum::DownMovement) { VerticleSway = false; }
+}
+
+// Called when the game starts or when spawned
+void ARedBloodCell::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ARedBloodCell::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!InWater) {
+		switch (CellMovementDirection) {
+		case MovementDirectionEnum::LeftMovement: this->AddActorWorldOffset(FVector(Speed * -1, 0, 0)); break;
+		case MovementDirectionEnum::UpMovement: this->AddActorWorldOffset(FVector(0, 0, Speed)); break;
+		case MovementDirectionEnum::RightMovement: this->AddActorWorldOffset(FVector(Speed, 0, 0)); break;
+		case MovementDirectionEnum::DownMovement: this->AddActorWorldOffset(FVector(0, 0, Speed * -1)); break;
+		case MovementDirectionEnum::NoMovement: break;
+		}
+	}else
+	{
+		if (this->GetActorLocation().Z < 1130) {
+			this->AddActorWorldOffset(FVector(0, 0, 5));
+		}
+	}
+
+
+	if (VerticleSway && !InWater) {
+		if (VerticleSwayCount < VerticleSwayDestination && VerticleSwayDestination > 0)
+		{
+			this->AddActorWorldOffset(FVector(0, 0, 1));
+			VerticleSwayCount++;
+		}
+		else if (VerticleSwayCount > VerticleSwayDestination && VerticleSwayDestination < 0)
+		{
+			this->AddActorWorldOffset(FVector(0, 0, -1));
+			VerticleSwayCount--;
+		}
+		else if (VerticleSwayCount == VerticleSwayDestination)
+		{
+			VerticleSwayDestination *= -1;
+		}
+	}
+	if (HorizontalSway && !InWater) {
+		if (HorizontalSwayCount < HorizontalSwayDestination && HorizontalSwayDestination > 0)
+		{
+			this->AddActorWorldOffset(FVector(1, 0, 0));
+			HorizontalSwayCount++;
+		}
+		else if (HorizontalSwayCount > HorizontalSwayDestination && HorizontalSwayDestination < 0)
+		{
+			this->AddActorWorldOffset(FVector(-1, 0, 0));
+			HorizontalSwayCount--;
+
+		}
+		else if (HorizontalSwayCount == HorizontalSwayDestination)
+		{
+			HorizontalSwayDestination *= -1;
+		}
+	}
+
+	
+}
+
+void ARedBloodCell::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	if (OtherActor->GetName() == "Water_Blueprint")
+	{
+		InWater = true;
+	}
+}
---
+++ Source/BloodGame/WhiteBloodCell.h
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "Engine.h"
+#include "RedBloodCell.h"
+#include "WhiteBloodCell.generated.h"
+
+UENUM()
+enum class ShieldOrientationEnum : uint8{
+	ShieldOrientation_Up UMETA(DisplayName = "Up"),
+	ShieldOrientation_Down UMETA(DisplayName = "Down"),
+	ShieldOrientation_Left UMETA(DisplayName = "Left"),
+	ShieldOrientation_Right UMETA(DisplayName = "Right"),
+};
+
+UCLASS()
+class BLOODGAME_API AWhiteBloodCell : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AWhiteBloodCell();
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		bool ApplyChanges = true;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		bool VerticleSway = false;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		int32 VerticleSwaySeverity = 20;
+
+	UPROPERTY()
+		int32 VerticleSwayDestination = VerticleSwaySeverity;
+
+	UPROPERTY()
+		int32 VerticleSwayCount = 0;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		bool HorizontalSway = false;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		int32 HorizontalSwaySeverity = 20;
+
+	UPROPERTY()
+		int32 HorizontalSwayDestination = HorizontalSwaySeverity;
+
+	UPROPERTY()
+		int32 HorizontalSwayCount = 0;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		ShieldOrientationEnum ShieldOrientation = ShieldOrientationEnum::ShieldOrientation_Left;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		MovementDirectionEnum CellMovementDirection = MovementDirectionEnum::NoMovement;
+
+	UPROPERTY()
+		bool IsMoving = true;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		float Speed = 5.0f;
+
+	UPROPERTY()
+		bool Collided = false;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		UPaperSprite * Sprite;
+
+	UPROPERTY()
+		UPaperSpriteComponent * CellSprite;
+
+	UPROPERTY(EditAnywhere, Category = "White Blood Cell")
+		UStaticMesh * Shield;
+
+	UPROPERTY()
+		UInstancedStaticMeshComponent * ShieldISM;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	// Called every time anything is changed
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// Called when the cell hits anything
+	UFUNCTION()
+		void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+};

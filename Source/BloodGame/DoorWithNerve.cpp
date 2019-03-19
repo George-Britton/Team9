df89d1d6b0062dc58a74224da402944dff6c995e
George Britton <george.britton@student.nua.ac.uk>
2019-03-19 19:56:38 +0000

    Documented the doors

--- Source/BloodGame/DoorWithNerve.cpp
+++ Source/BloodGame/DoorWithNerve.cpp
@@ -9,19 +9,25 @@ ADoorWithNerve::ADoorWithNerve()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Set the basic objects for the actor's components
 	this->RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	this->LeftDoor = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Left Door"));
 	this->RightDoor = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Right Door"));
 	this->NerveISM = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Nerve"));
 
+	// Sets the attachments to the root
 	LeftDoor->SetupAttachment(this->RootComponent);
 	RightDoor->SetupAttachment(this->RootComponent);
 	NerveISM->SetupAttachment(this->RootComponent);
 }
 
+// Called when the doors or nerve hits anything
 void ADoorWithNerve::OnConstruction(const FTransform& Transform)
 {
+	// Makes sure it spawns at least one set of doors
 	if (AmountOfDoors < 1){ AmountOfDoors = 1; }
+
+	// Sets the ISM meshes
 	if (this->Door) {
 		this->LeftDoor->SetStaticMesh(Door);
 		this->RightDoor->SetStaticMesh(Door);
@@ -30,12 +36,15 @@ void ADoorWithNerve::OnConstruction(const FTransform& Transform)
 		this->NerveISM->SetStaticMesh(Nerve);
 	}
 
+	// Changes to be made when applied
 	if (Apply) {
+		// Clear all the ISMs
 		LeftDoor->ClearInstances();
 		RightDoor->ClearInstances();
 		NerveISM->ClearInstances();
+		
+		// Makes sure there's a customisable transform for each set of doors
 		FTransform BaseTransform;
-
 		if (AmountOfDoors < DoorTransforms.Num()){ for (int32 Popper = 0; Popper < (DoorTransforms.Num() - AmountOfDoors); Popper++) { DoorTransforms.Pop(); } }
 		else if (AmountOfDoors > DoorTransforms.Num())
 		{
@@ -47,6 +56,7 @@ void ADoorWithNerve::OnConstruction(const FTransform& Transform)
 
 		if (Door && Nerve)
 		{
+			// Places all the doors and nerve isms in the world
 			for (int32 Placer = 0; Placer < AmountOfDoors; Placer++)
 			{
 				FTransform DoorPlacement = DoorTransforms[Placer];
@@ -59,7 +69,7 @@ void ADoorWithNerve::OnConstruction(const FTransform& Transform)
 			NerveISM->AddInstance(NerveLocation);
 		}else
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, TEXT("Please assign static meshes to both 'Door' and 'Nerve'"));
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, TEXT("Please assign static meshes to both 'Door' and 'Nerve'")); // Warning if the meshes don't exist
 		}
 
 		Apply = false;
@@ -73,6 +83,7 @@ void ADoorWithNerve::Tick(float DeltaTime)
 
 	if (DoorState == DoorStateEnum::DoorState_Closing)
 	{
+		// Closes the doors slightly ever tick
 		for (int32 DoorIncrementer = 0; DoorIncrementer < AmountOfDoors; DoorIncrementer++)
 		{
 			FTransform DoorNewTransform;
@@ -88,6 +99,7 @@ void ADoorWithNerve::Tick(float DeltaTime)
 
 		DoorCounter--;
 
+		// Changes the door's state to closed and puts the nerve back in the scene
 		if (!DoorCounter)
 		{
 			DoorState = DoorStateEnum::DoorState_Closed;
@@ -96,6 +108,7 @@ void ADoorWithNerve::Tick(float DeltaTime)
 		}
 	}
 	
+	// Waits for the predetermined amount of time then changes the door's state to closing
 	if (DoorState == DoorStateEnum::DoorState_Open && Retriggerable)
 	{
 		DoorOpenDelay += DeltaTime;
@@ -106,6 +119,7 @@ void ADoorWithNerve::Tick(float DeltaTime)
 
 	if (DoorState == DoorStateEnum::DoorState_Opening)
 	{
+		// Opens the doors slightly every tick
 		for (int32 DoorIncrementer = 0; DoorIncrementer < AmountOfDoors; DoorIncrementer++)
 		{
 			FTransform DoorNewTransform;
@@ -121,6 +135,7 @@ void ADoorWithNerve::Tick(float DeltaTime)
 
 		DoorCounter++;
 
+		// Changes the door's state to open
 		if (DoorCounter == 25)
 		{
 			DoorState = DoorStateEnum::DoorState_Open;
@@ -133,6 +148,7 @@ void ADoorWithNerve::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrim
 {
 	if ((Other != NULL) && (Other != this) && (OtherComp != NULL))
 	{
+		// Checks if the player hit the nerve, removes the nerve, and changes the door's state to opening
 		if (Other->GetName()=="Player" && MyComp == NerveISM && DoorState == DoorStateEnum::DoorState_Closed)
 		{
 			DoorState = DoorStateEnum::DoorState_Opening;
--- Source/BloodGame/DoorWithNerve.h
+++ Source/BloodGame/DoorWithNerve.h
@@ -7,7 +7,7 @@
 #include "Engine.h"
 #include "DoorWithNerve.generated.h"
 
-UENUM()
+UENUM() // Enum for the movement state of the doors
 enum class DoorStateEnum : uint8 {
 	DoorState_Opening UMETA(DisplayName = "Opening"),
 	DoorState_Closing UMETA(DisplayName = "Closing"),
@@ -24,45 +24,59 @@ public:
 	// Sets default values for this actor's properties
 	ADoorWithNerve();
 
+	// Makes the changes to the actor when true
 	UPROPERTY(EditAnywhere, Category = "Doors")
 		bool Apply = false;
 
+	// Whether or not the door closes again
 	UPROPERTY(EditAnywhere, Category = "Doors")
 		bool Retriggerable = false;
 
+	// Number of sets of doors to spawn
 	UPROPERTY(EditAnywhere, Category = "Doors")
 		int32 AmountOfDoors = 1;
 
+	// How long the doors stay open in seconds
 	UPROPERTY(EditAnywhere, Category = "Doors")
 		uint32 DoorsOpenTime = 2;
 
+	// Static mesh for the door ISMs
 	UPROPERTY(EditAnywhere, Category = "Doors")
 		UStaticMesh * Door;
 
+	// Static mesh for the NerveISM
 	UPROPERTY(EditAnywhere, Category = "Doors")
 		UStaticMesh * Nerve;
 
+	// Transform of the nerve that controls the doors
 	UPROPERTY(EditAnywhere, Category = "Doors")
 		FTransform NerveLocation;
 
+	// Transforms of all the sets of doors
 	UPROPERTY(EditAnywhere, Category = "Doors")
 		TArray<FTransform> DoorTransforms;
 
+	// Counter for opening and closing the doors
 	UPROPERTY()
 		uint32 DoorCounter = 0;
 
+	// How long the doors have been open
 	UPROPERTY()
 		float DoorOpenDelay = 0;
 
+	// Instanced static mesh of the left door
 	UPROPERTY()
 		UInstancedStaticMeshComponent * LeftDoor;
 
+	// Instanced static mesh of the right door
 	UPROPERTY()
 		UInstancedStaticMeshComponent * RightDoor;
 
+	// Instanced static mesh of the nerve
 	UPROPERTY()
 		UInstancedStaticMeshComponent * NerveISM;
 
+	// Enum of the movement state of the doors
 	UPROPERTY()
 		DoorStateEnum DoorState = DoorStateEnum::DoorState_Closed;
 
@@ -70,6 +84,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	// Called every time anything is changed
 	virtual void OnConstruction(const FTransform& Transform) override;
 
 private:

09fe78424a8f2db73a70bfc35a4fbe1fd7f52d80
George Britton <george.britton@student.nua.ac.uk>
2019-05-08 23:43:20 +0100

    Adding sound cues and sprite to flipbook conversion

--- Source/BloodGame/WhiteBloodCell.cpp
+++ Source/BloodGame/WhiteBloodCell.cpp
@@ -12,13 +12,15 @@ AWhiteBloodCell::AWhiteBloodCell()
 
 	// Set the basic objects for the actor's components
 	this->RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
-	this->ShieldISM = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Shield"));
-	this->CellSprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("Cell Sprite"));
+	this->ShieldSprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("Shield"));
+	this->CellSprite = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("Cell Sprite"));
+	this->ShieldBounceAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("Audio"));
 
 	// Sets the attachments to the root
 	CellSprite->SetupAttachment(RootComponent);
-	ShieldISM->SetupAttachment(RootComponent);
-	ShieldISM->SetNotifyRigidBodyCollision(true);
+	ShieldSprite->SetupAttachment(RootComponent);
+	ShieldSprite->SetNotifyRigidBodyCollision(true);
+	ShieldBounceAudioComponent->SetupAttachment(RootComponent);
 }
 
 // Called every time anything changes
@@ -29,41 +31,46 @@ void AWhiteBloodCell::OnConstruction(const FTransform& Transform)
 	if (!HorizontalSwaySeverity) { HorizontalSwaySeverity = 20; }
 	uint8 RandomVerticalDirection = FMath::RandRange(0, 1);
 	uint8 RandomHorizontalDirection = FMath::RandRange(0, 1);
-	if (RandomVerticalDirection){ VerticalSwayDestination = VerticalSwaySeverity; }
-	else{ VerticalSwayDestination = VerticalSwaySeverity * -1; }
-	if (RandomHorizontalDirection){ HorizontalSwayDestination = HorizontalSwaySeverity;	}
+	if (RandomVerticalDirection) { VerticalSwayDestination = VerticalSwaySeverity; }
+	else { VerticalSwayDestination = VerticalSwaySeverity * -1; }
+	if (RandomHorizontalDirection) { HorizontalSwayDestination = HorizontalSwaySeverity; }
 	else { HorizontalSwayDestination = HorizontalSwaySeverity * -1; }
 
 	// Changes to be made when applied
 	if (ApplyChanges) {
-		if (this->Sprite)
+		if (this->SpriteFlipbook && this->CellSprite)
 		{
 			// Creates the sprite for the cell
-			CellSprite->SetSprite(Sprite);
-			CellSprite->SetWorldScale3D(FVector(0.75, 1, 0.75));
-			CellSprite->SetWorldRotation(FRotator((FMath::RandRange(-179, 180)), 0, 0).Quaternion());
+			CellSprite->SetFlipbook(SpriteFlipbook);
 		}
 		else
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, TEXT("Please allocate a sprite")); // Warning if the sprite doesn't exist
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, TEXT("Please allocate a sprite flipbook")); // Warning if the sprite doesn't exist
 		}
 
 		// Sets the order the cell should be rendered
-		CellSprite->TranslucencySortPriority = 75;
+		// CellSprite->TranslucencySortPriority = 75;
 
 		if (this->Shield)
 		{
 			// Respawns the shield in the orientation decided in the editor
-			ShieldISM->ClearInstances();
-			this->ShieldISM->SetStaticMesh(this->Shield);
+			ShieldSprite->SetSprite(Shield);
 			FTransform ShieldTransform;
-			switch(ShieldOrientation){
+			switch (ShieldOrientation) {
 			case ShieldOrientationEnum::ShieldOrientation_Left: ShieldTransform.SetLocation(FVector(-100, 0, 0)); ShieldTransform.SetRotation((FRotator(0, 0, 0).Quaternion())); break;
 			case ShieldOrientationEnum::ShieldOrientation_Right: ShieldTransform.SetLocation(FVector(100, 0, 0)); ShieldTransform.SetRotation((FRotator(180, 0, 0).Quaternion())); break;
 			case ShieldOrientationEnum::ShieldOrientation_Up: ShieldTransform.SetLocation(FVector(0, 0, 100)); ShieldTransform.SetRotation((FRotator(270, 0, 0).Quaternion())); break;
 			case ShieldOrientationEnum::ShieldOrientation_Down: ShieldTransform.SetLocation(FVector(0, 0, -100)); ShieldTransform.SetRotation((FRotator(90, 0, 0).Quaternion())); break;
 			}
-			ShieldISM->AddInstance(ShieldTransform);
+			ShieldSprite->SetRelativeTransform(ShieldTransform);
+		}
+
+		if (ShieldBounceSound) {
+			// Sets the sound that will be played on shield bounce
+			ShieldBounceAudioComponent->SetSound(ShieldBounceSound);
+		}
+		else {
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, TEXT("Please allocate a sound")); // Warning if the sound doesn't exist
 		}
 
 		ApplyChanges = false;
@@ -83,7 +90,8 @@ void AWhiteBloodCell::BeginPlay()
 	if (CellMovementDirection == MovementDirectionEnum::NoMovement)
 	{
 		IsMoving = false;
-	}else
+	}
+	else
 	{
 		IsMoving = true;
 	}
@@ -155,17 +163,20 @@ void AWhiteBloodCell::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPri
 	{
 		// Creates a reference to the player
 		APlayerPawn * PlayerRef = Cast<APlayerPawn>(Other);
-		if(PlayerRef && MyComp == ShieldISM)
+		if (PlayerRef && MyComp == ShieldSprite)
 		{
+			if (ShieldBounceSound && !(CellMovementDirection == MovementDirectionEnum::NoMovement)) {
+				ShieldBounceAudioComponent->Play();
+			}
+
 			MovementReset = false;
 
-			FTransform ShieldLocation;
-			ShieldISM->GetInstanceTransform(0, ShieldLocation, true);
+			FTransform ShieldLocation = ShieldSprite->GetRelativeTransform();
 
 			// Tells the player they've been hit by a shield
 			TempCellDirection = CellMovementDirection;
 			CellMovementDirection = MovementDirectionEnum::NoMovement;
-			PlayerRef->HitShield(this, BounceStrength, ShieldLocation.GetLocation());
+			PlayerRef->HitShield(this, BounceStrength, TempCellDirection);
 
 			//Stuns and restarts the cell
 			FTimerHandle MovementRestartTimer;
